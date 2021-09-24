6f34d6269b8d43aaaccb52d93dbb7d24e65dd40a
Virendra Jhala <virendrapratapsinghjhala@gmail.com>
2021-09-24 12:50:18 +0530

    completed development of UserService class

--- src/main/java/org/freton/usermanager/service/UserService.kt
+++ src/main/java/org/freton/usermanager/service/UserService.kt
@@ -2,14 +2,60 @@ package org.freton.usermanager.service
 
 import org.freton.usermanager.model.UserModel
 import org.freton.usermanager.repository.UserRepository
+import org.freton.usermanager.userexception.UserException
 
-val user = UserRepository()
+val userRepo = UserRepository()
 
 class UserService {
 
     fun getUsers() : MutableList<UserModel>{
 
-        var userList = user.getUsers()
+        var userList = userRepo.getUsers()
+        return userList
+    }
 
+    fun addUser(user:UserModel):UserModel{
+
+        try {
+           val addedUSer = userRepo.addUser(user)
+            return addedUSer
+        }
+
+        catch(e: UserException){
+            throw e
+        }
+    }
+
+    fun updateUser(user:UserModel):UserModel?{
+
+        try {
+            val updatedUser = userRepo.updateUser(user)
+            return updatedUser
+        }
+
+        catch(e: UserException){
+            throw e
+        }
+    }
+
+    fun deleteUser(id:Long): Boolean{
+
+        try {
+          val isUserDeleted = userRepo.deleteUser(id)
+            return isUserDeleted
+        }
+
+        catch(e: UserException){
+            throw e
+        }
+
+        catch(e:NullPointerException){
+            throw e
+        }
+
+        catch(e:UnsupportedOperationException)
+        {
+            throw e
+        }
     }
 }
