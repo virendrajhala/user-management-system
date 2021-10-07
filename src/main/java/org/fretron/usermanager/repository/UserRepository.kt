01d944e242ab710955115276879486bed8327f8d
Virendra Jhala <virendrapratapsinghjhala@gmail.com>
2021-10-07 12:51:25 +0530

    Removed redundant code from UserRepository get methods and updated code for Update method

--- src/main/java/org/fretron/usermanager/repository/UserRepository.kt
+++ src/main/java/org/fretron/usermanager/repository/UserRepository.kt
@@ -1,37 +1,38 @@
 package org.freton.usermanager.repository
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.mongodb.BasicDBObject
+import com.mongodb.DBObject
 import com.mongodb.MongoClient
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Filters
-import com.mongodb.client.model.Updates
+import com.mongodb.util.JSON
 import org.bson.Document
+import org.bson.json.JsonObject
 import org.fretron.usermanager.model.UserModel
-import java.io.*
-
 
 class UserRepository {
 
     private val mongo : MongoClient
     private val db : MongoDatabase
+    private val mapper : ObjectMapper
     init{
 
         mongo = MongoClient("localhost", 27017)
         db = mongo.getDatabase("UserManager")
+        mapper = jacksonObjectMapper()
     }
 
     fun getUsers() : MutableList<UserModel>{
 
         val dbCursor = db.getCollection("User").find().iterator()
         val userList = mutableListOf<UserModel>()
+
         while(dbCursor.hasNext()){
             val dbObject1 = dbCursor.next()
-            val id = dbObject1["_id"]
-            val name = dbObject1["name"].toString()
-            val address = dbObject1["address"].toString()
-            val email = dbObject1["email"].toString()
-            val age = dbObject1["age"]
 
-            val user = UserModel(id as Int?,name,address,email, age as Int?)
+            val user = mapper.readValue(JsonObject(JSON.serialize(dbObject1)).toString(),UserModel::class.java)
             userList.add(user)
         }
 
@@ -47,13 +48,8 @@ class UserRepository {
         while(cursor.hasNext()) {
             val dbObject1 = cursor.next()
 
-            val id = dbObject1["_id"]
-            val name = dbObject1["name"].toString()
-            val address = dbObject1["address"].toString()
-            val email = dbObject1["email"].toString()
-            val age = dbObject1["age"]
+            val user = mapper.readValue(JsonObject(JSON.serialize(dbObject1)).toString(),UserModel::class.java)
 
-            val user = UserModel(id as Int?, name, address, email, age as Int?)
             userList.add(user)
         }
 
@@ -69,13 +65,9 @@ class UserRepository {
 
         while(cursor.hasNext()){
             val dbObject1 = cursor.next()
-            val id = dbObject1["_id"]
-            val name = dbObject1["name"].toString()
-            val address = dbObject1["address"].toString()
-            val email = dbObject1["email"].toString()
-            val age = dbObject1["age"]
 
-            val user = UserModel(id as Int?,name,address,email, age as Int?)
+            val user = mapper.readValue(JsonObject(JSON.serialize(dbObject1)).toString(),UserModel::class.java)
+
             userList.add(user)
         }
 
@@ -91,13 +83,9 @@ class UserRepository {
 
         while(dbCursor.hasNext()){
             val dbObject1 = dbCursor.next()
-            val id = dbObject1["_id"]
-            val name = dbObject1["name"].toString()
-            val address = dbObject1["address"].toString()
-            val email = dbObject1["email"].toString()
-            val age = dbObject1["age"]
 
-            val user = UserModel(id as Int?,name,address,email, age as Int?)
+            val user = mapper.readValue(JsonObject(JSON.serialize(dbObject1)).toString(),UserModel::class.java)
+
             userList.add(user)
         }
 
@@ -122,23 +110,24 @@ class UserRepository {
 
         val collection = db.getCollection("User")
         val filter = Filters.eq("_id",user.`id$1`)
-        collection.updateOne(filter,Updates.set("name",user.getName()))
-        collection.updateOne(filter,Updates.set("address",user.getAddress()))
-        collection.updateOne(filter,Updates.set("email",user.getEmail()))
-        collection.updateOne(filter,Updates.set("age",user.getAge()))
+
+        val doc = Document.parse(user.toString())
+
+        // set doc id = our object id
+      //  doc["_id"] = user.`id$1`
+        val updateDoc = BasicDBObject()
+        updateDoc["\$set"] = doc
+
+        collection.updateOne(filter,updateDoc)
 
         val dbCursor = collection.find(filter).iterator()
         val userList = mutableListOf<UserModel>()
         while(dbCursor.hasNext()){
 
             val dbObject1 = dbCursor.next()
-            val id = dbObject1["_id"]
-            val name = dbObject1["name"].toString()
-            val address = dbObject1["address"].toString()
-            val email = dbObject1["email"].toString()
-            val age = dbObject1["age"]
 
-            val user = UserModel(id as Int?,name,address,email, age as Int?)
+            val user = mapper.readValue(JsonObject(JSON.serialize(dbObject1)).toString(),UserModel::class.java)
+
             userList.add(user)
         }
         return userList.first()
@@ -152,5 +141,16 @@ class UserRepository {
         val result = collection.deleteOne(filter)
 
         return result != null
+    }
+
+    fun getUserModel(dbObject1: DBObject) : UserModel{
+        val id = dbObject1["_id"]
+        val name = dbObject1["name"].toString()
+        val address = dbObject1["address"].toString()
+        val email = dbObject1["email"].toString()
+        val age = dbObject1["age"]
+
+        val user = UserModel(id as Int?,name,address,email, age as Int?)
+        return user
     }
 }
